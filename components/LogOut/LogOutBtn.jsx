e6a2ab81f5dd46528b1d897f2b379e17eebee0f0
Rd <97230275+rdghostmann@users.noreply.github.com>
2025-02-24 05:52:15 +0100

    Update LogOutBtn.jsx to clear store

--- components/LogOut/LogOutBtn.jsx
+++ components/LogOut/LogOutBtn.jsx
@@ -2,12 +2,15 @@
 
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useUserStore } from "../../store/useUserStore";
 
 export default function LogOutBtn() {
   const router = useRouter();
+  const clearUser = useUserStore((state) => state.clearUser);
 
   const handleLogout = async () => {
     await signOut({ redirect: false }); // Prevent NextAuth default redirection
+    clearUser(); // Clear the user data from the store
     router.push("/login"); // Manually redirect to login page
   };
 
