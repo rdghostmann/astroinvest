aa388e30f55985006593a64cadfc3e8775b0aed3
unknown <randalchukzwilson@gmail.com>
2025-03-07 12:21:35 +0100

    fixed LogOut and Stuff 2

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -30,7 +30,7 @@ export default async function Page() {
               <BreadcrumbList>
                 <BreadcrumbItem className="hidden md:block">
                   <BreadcrumbLink href="#">
-                    InvestJar
+                    AstroInvest
                   </BreadcrumbLink>
                 </BreadcrumbItem>
                 <BreadcrumbSeparator className="hidden md:block" />
