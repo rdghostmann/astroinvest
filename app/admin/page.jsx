54c0b29f7de5426253ca49b2fa293f5d070be933
unknown <randalchukzwilson@gmail.com>
2025-03-10 18:30:50 +0100

    Added Admin route2

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,15 +1,59 @@
-
-import LogOutBtn from '@/components/LogOut/LogOutBtn'
 import React from 'react'
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+import LogOutBtn from '@/components/LogOut/LogOutBtn'
 
 const page = () => {
   return (
-    <div>
-      <h3>Admin Page</h3>
-      <LogOutBtn />
-      
-    </div>
-  )
+    <SidebarProvider>
+      {/* <AppSidebarWrapper /> */}
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    AstroInvest
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Admin Dashboard</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          {/* Left Section */}
+          <div className="flex-1 space-y-6 basis-0">
+            {/* Portfolio Overview */}
+            <h2 className="text-lg font-semibold">Admin Dashboard</h2>
+            <LogOutBtn />
+          </div>
+
+
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
 }
 
 export default page
