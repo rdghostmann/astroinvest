ab6e00980eb17511a032009a25994f7eaf154b7e
unknown <randalchukzwilson@gmail.com>
2025-03-10 18:26:37 +0100

    Added Admin route

---
+++ app/admin/page.jsx
@@ -0,0 +1,15 @@
+
+import LogOutBtn from '@/components/LogOut/LogOutBtn'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h3>Admin Page</h3>
+      <LogOutBtn />
+      
+    </div>
+  )
+}
+
+export default page
