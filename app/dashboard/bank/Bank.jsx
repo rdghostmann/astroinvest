2667e383bfcd55a2445097142b4f53005f6ef232
unknown <randalchukzwilson@gmail.com>
2025-03-11 10:24:44 +0100

    Updatding Bank

--- app/dashboard/bank/Bank.jsx
+++ app/dashboard/bank/Bank.jsx
@@ -61,9 +61,15 @@ export default function Bank() {
 
       {showPopup && (
         <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
+          <button
+            className="absolute border rounded-full top-2 right-2 text-gray-500 hover:text-gray-700"
+            onClick={() => setShowPopup(false)}
+          >
+            <XIcon className="h-6 w-6" />
+          </button>
           <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full relative">
             <button
-              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+              className="absolute border rounded-full top-2 right-2 text-gray-500 hover:text-gray-700"
               onClick={() => setShowPopup(false)}
             >
               <XIcon className="h-6 w-6" />
