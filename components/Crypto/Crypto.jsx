312449f844674fcab7b4d9a777fc9ff63684c423
unknown <randalchukzwilson@gmail.com>
2025-03-14 11:18:08 +0100

    Added Doge

--- components/Crypto/Crypto.jsx
+++ components/Crypto/Crypto.jsx
@@ -4,6 +4,7 @@ import bitcoinImage from '../../public/crypto/btc.png';
 import ethereumImage from '../../public/crypto/eth.png';
 import solanaImage from '../../public/crypto/sol.png';
 import xrpImage from '../../public/crypto/xrp.png';
+import dogeImage from '../../public/crypto/doge.png';
 import bnbImage from '../../public/crypto/bnb.png';
 
 // Array of cryptocurrency objects
@@ -12,7 +13,8 @@ const cryptos = [
   { id: 2, name: 'Ethereum', path: ethereumImage },
   { id: 3, name: 'Solana', path: solanaImage },
   { id: 4, name: 'XRP', path: xrpImage },
-  { id: 5, name: 'BNB', path: bnbImage },
+  { id: 5, name: 'DOGE', path: dogeImage },
+  { id: 6, name: 'BNB', path: bnbImage },
 ];
 
 // Export the image paths
@@ -21,6 +23,7 @@ export const cryptoImages = {
   Ethereum: ethereumImage,
   Solana: solanaImage,
   XRP: xrpImage,
+  Doge: dogeImage,
   BNB: bnbImage,
 };
 
