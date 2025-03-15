3a3f6a6395503a1b7797063b62e3540e53bad200
unknown <randalchukzwilson@gmail.com>
2025-03-15 13:32:41 +0100

    Updated Stuff 6i

--- components/CardDetail/CardDetail.jsx
+++ components/CardDetail/CardDetail.jsx
@@ -105,13 +105,11 @@ export default function CardDetail({ userID }) {
 
       {showPopup && (
         <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
+          <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+            onClick={() => setShowPopup(false)} >
+            <XIcon className="h-6 w-6" />
+          </button>
           <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full relative">
-            <button
-              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
-              onClick={() => setShowPopup(false)}
-            >
-              <XIcon className="h-6 w-6" />
-            </button>
             {/* Card Details Form */}
             <Card>
               <CardHeader>
