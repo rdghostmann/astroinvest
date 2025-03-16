efea342e66069536138c964786c281a22fd07481
unknown <randalchukzwilson@gmail.com>
2025-03-16 06:22:48 +0100

    Updating Features

--- app/admin/users/page.jsx
+++ app/admin/users/page.jsx
@@ -1,57 +1,11 @@
-import React, { useState } from 'react';
-import { getAllUsers } from '@/lib/actions'; // Import the server action
-import Link from 'next/link';
+import React from 'react'
+import UsersPage from './UsersPage'
 
-const UsersPage = async () => {
-  const users = await getAllUsers(); // Fetch all users
 
-  const [searchTerm, setSearchTerm] = useState('');
-  const [filteredUsers, setFilteredUsers] = useState(users);
-
-  const handleSearch = (event) => {
-    const term = event.target.value.toLowerCase();
-    setSearchTerm(term);
-
-    const filtered = users.filter((user) =>
-      user.username.toLowerCase().includes(term) ||
-      user.email.toLowerCase().includes(term)
-    );
-    setFilteredUsers(filtered);
-  };
-
+export default function page() {
   return (
     <div>
-      <h2 className="font-semibold">Users List</h2>
-      <input
-        type="text"
-        placeholder="Search by username or email"
-        value={searchTerm}
-        onChange={handleSearch}
-        className="mb-4 p-2 border rounded"
-      />
-      <div className="grid grid-cols-1 gap-4">
-        {filteredUsers.length > 0 ? (
-          filteredUsers.map((user) => (
-            <div key={user._id} className="flex items-center gap-4 p-4 border rounded-lg shadow-md">
-              <div className="flex flex-col">
-                <span className="font-semibold">Username: {user.username}</span>
-                <span>Email: {user.email}</span>
-                <span>Wallet Address: {user.walletAddress}</span>
-                <span>Profit Total: ${user.profitTotal}</span>
-                <span>Role: {user.role}</span>
-                <span>Verified: {user.isVerified ? 'Yes' : 'No'}</span>
-                <Link href={`/admin/users/${user._id}`}>
-                  <a className="text-blue-500">View Wallet</a>
-                </Link>
-              </div>
-            </div>
-          ))
-        ) : (
-          <div>No users found</div>
-        )}
-      </div>
+      <UsersPage />
     </div>
-  );
-};
-
-export default UsersPage;
+  )
+}
