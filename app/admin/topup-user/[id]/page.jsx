c8f0cfa2dacade1b7c91fbd273fc24596a5c9b98
unknown <randalchukzwilson@gmail.com>
2025-03-16 09:07:51 +0100

    Updating User Wallet Singleton

--- app/admin/topup-user/[id]/page.jsx
+++ app/admin/topup-user/[id]/page.jsx
@@ -1,42 +1,72 @@
-import React from 'react';
+import React from 'react'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+import { AdminSidebar } from '../_component/AdminSidebar'
+import UsersCard from './UsersCard';
+
 import { getUserById, updateUserWallet } from '@/lib/actions'; // Import the server actions
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 
-export default async function UserWallet ({ params }) {
+const page = async () => {
   const { id } = params;
   const user = await getUserById(id);
 
-  console.log("The User Id", user);
-
 
   return (
-    <div>
-      <h2 className="font-semibold">User Wallets</h2>
-      <p>User ID: {user}</p>
-      {/* <ul role="list" className="grid grid-cols-1 gap-4 divide-y divide-gray-100">
-        {wallets.map((wallet) => (
-          <li key={wallet._id} className="flex items-center gap-4 p-4 border rounded-lg shadow-md">
-            <div className="flex justify-between gap-x-6 py-5">
-              <span className="font-semibold">{wallet.name}</span>
-              <span>Balance: ${wallet.balance}</span>
-              <Input
-                type="number"
-                value={wallet.balance}
-                onChange={(e) => handleUpdate(wallet._id, e.target.value)}
-                className="mt-2 p-2 border rounded"
-              />
-              <Button
-                onClick={() => handleUpdate(wallet._id, wallet.balance)}
-                className="mt-2 p-2 bg-blue-500 text-white rounded"
-              >
-                Update Balance
-              </Button>
-            </div>
-          </li>
-        ))}
-      </ul> */}
-    </div>
+    <SidebarProvider>
+      <AdminSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    AstroInvest
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Top User's Wallet</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-1 p-4 pt-0">
+          {/* Left Section */}
+          {/* <div className="flex-1 space-y-6 basis-0"> */}
+          {/* Portfolio Overview */}
+          {/* <h2 className="text-lg font-semibold">TopUp Users</h2> */}
+          {/* </div> */}
+          <div>
+            <h2 className="font-semibold">User Wallets</h2>
+            <p>User ID: {user}</p>
+
+          </div>
+
+
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
-};
+}
+
+export default page
 
