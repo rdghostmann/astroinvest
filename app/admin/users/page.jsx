455437e929ee50a656c887513b99b231c2ad5f2e
unknown <randalchukzwilson@gmail.com>
2025-03-16 06:48:56 +0100

    Added Users Table to Admin

--- app/admin/users/page.jsx
+++ app/admin/users/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import UsersCard from './UsersCard'
+// import UsersCard from './UsersCard'
+import UsersTable from '../_component/UsersTable'
 
 
 export default function page() {
   return (
     <div>
-      <UsersCard />
+      {/* <UsersCard /> */}
+      <UsersTable />
     </div>
   )
 }
