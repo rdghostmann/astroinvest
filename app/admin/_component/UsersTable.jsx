0842497f9a5825721e8f00866e8a088206717f7e
unknown <randalchukzwilson@gmail.com>
2025-03-16 08:46:52 +0100

    Updated Stuffs3 ii

--- app/admin/_component/UsersTable.jsx
+++ app/admin/_component/UsersTable.jsx
@@ -27,7 +27,7 @@ const UsersTable = ({ users }) => {
 
   return (
     <div>
-      <div className="flex items-center justify-between mb-4">
+      <div className="grid grid-col-2 mb-4">
         <h2 className="text-lg font-semibold">Users Lisiting</h2>
 
         <Input
@@ -38,42 +38,44 @@ const UsersTable = ({ users }) => {
           className="mb-4 p-2 border rounded"
         />
       </div>
-      <div className="flex flex-col overflow-x-auto">
-        <div className="sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-            <div className="rounded-t-lg overflow-x-auto border">
-              <Table className="min-w-full text-start text-sm font-light">
-                <TableHeader>
-                  <TableRow className="border-b border-neutral-200 bg-blue-950/50 hover:bg-blue-900/50">
-                    <TableHead className="text-blue-100 px-6 py-4">#</TableHead>
-                    <TableHead className="text-blue-100 px-6 py-4">Username</TableHead>
-                    <TableHead className="text-blue-100 px-6 py-4">Email</TableHead>
-                    <TableHead className="text-blue-100 px-6 py-4">Phone</TableHead>
-                    <TableHead className="text-blue-100 px-6 py-4">Country</TableHead>
-                    <TableHead className="text-blue-100 px-6 py-4">State</TableHead>
-                    <TableHead className="text-blue-100 px-6 py-4">Role</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {filteredUsers.length > 0 ? (
-                    filteredUsers.map((user, index) => (
-                      <TableRow key={user._id} className="border-b border-neutral-200 hover:bg-blue-900/20">
-                        <TableCell className="text-gray-600 px-6 py-4">{index + 1}</TableCell>
-                        <TableCell className="text-gray-600 px-6 py-4">{user.username}</TableCell>
-                        <TableCell className="text-gray-600 px-6 py-4">{user.email}</TableCell>
-                        <TableCell className="text-gray-600 px-6 py-4">{user.phone}</TableCell>
-                        <TableCell className="text-gray-600 px-6 py-4">{user.country}</TableCell>
-                        <TableCell className="text-gray-600 px-6 py-4">{user.state}</TableCell>
-                        <TableCell className="text-gray-600 px-6 py-4">{user.role}</TableCell>
-                      </TableRow>
-                    ))
-                  ) : (
-                    <TableRow>
-                      <TableCell colSpan="7" className="text-center text-gray-600 px-6 py-4">No users found</TableCell>
+      <div className="grid grid-col-1">
+        <div className="flex flex-col overflow-x-auto">
+          <div className="sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+              <div className="rounded-t-lg overflow-x-auto border">
+                <Table className="min-w-full text-start text-sm font-light">
+                  <TableHeader>
+                    <TableRow className="border-b border-neutral-200 bg-blue-950/50 hover:bg-blue-900/50">
+                      <TableHead className="text-blue-100 px-6 py-4">#</TableHead>
+                      <TableHead className="text-blue-100 px-6 py-4">Username</TableHead>
+                      <TableHead className="text-blue-100 px-6 py-4">Email</TableHead>
+                      <TableHead className="text-blue-100 px-6 py-4">Phone</TableHead>
+                      <TableHead className="text-blue-100 px-6 py-4">Country</TableHead>
+                      <TableHead className="text-blue-100 px-6 py-4">State</TableHead>
+                      <TableHead className="text-blue-100 px-6 py-4">Role</TableHead>
                     </TableRow>
-                  )}
-                </TableBody>
-              </Table>
+                  </TableHeader>
+                  <TableBody>
+                    {filteredUsers.length > 0 ? (
+                      filteredUsers.map((user, index) => (
+                        <TableRow key={user._id} className="border-b border-neutral-200 hover:bg-blue-900/20">
+                          <TableCell className="text-gray-600 px-6 py-4">{index + 1}</TableCell>
+                          <TableCell className="text-gray-600 px-6 py-4">{user.username}</TableCell>
+                          <TableCell className="text-gray-600 px-6 py-4">{user.email}</TableCell>
+                          <TableCell className="text-gray-600 px-6 py-4">{user.phone}</TableCell>
+                          <TableCell className="text-gray-600 px-6 py-4">{user.country}</TableCell>
+                          <TableCell className="text-gray-600 px-6 py-4">{user.state}</TableCell>
+                          <TableCell className="text-gray-600 px-6 py-4">{user.role}</TableCell>
+                        </TableRow>
+                      ))
+                    ) : (
+                      <TableRow>
+                        <TableCell colSpan="7" className="text-center text-gray-600 px-6 py-4">No users found</TableCell>
+                      </TableRow>
+                    )}
+                  </TableBody>
+                </Table>
+              </div>
             </div>
           </div>
         </div>
