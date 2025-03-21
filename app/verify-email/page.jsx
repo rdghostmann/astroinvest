4b0dff2806ff112dca3a7d30f3ea933922bc6595
unknown <randalchukzwilson@gmail.com>
2025-03-21 15:30:44 +0100

    Updated Registration process II

--- app/verify-email/page.jsx
+++ app/verify-email/page.jsx
@@ -3,12 +3,13 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { CircleX, SquareCheckBig } from "lucide-react";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation"; 
 import { useToast } from "@/hooks/use-toast";
 import { verifyEmail } from "@/lib/verifyEmail";
 
 const VerifyEmail = () => {
   const { toast } = useToast();
+  const router = useRouter(); 
 
   const [loading, setLoading] = useState(false);
   const [verified, setVerified] = useState(false);
@@ -28,10 +29,19 @@ const VerifyEmail = () => {
     }
   }, []);
 
-  const verifyEmailHandler = async () => {
 
-    console.log("Verification Token:", verifyToken);
-    console.log("User ID:", id);
+  useEffect(() => {
+    if (verified) {
+      // Redirect to /login after 5 seconds
+      const timer = setTimeout(() => {
+        router.push("/login");
+      }, 5000);
+
+      return () => clearTimeout(timer); 
+    }
+  }, [verified, router]);
+
+  const verifyEmailHandler = async () => {
 
     if (!verifyToken || !id) {
       toast({ variant: "destructive", title: "Invalid URL" });
@@ -48,19 +58,16 @@ const VerifyEmail = () => {
         setLoading(false);
         setVerified(true);
         toast({ title: result.message });
-        console.log("Verified");
       } else {
         setError(true);
         setLoading(false);
         toast({ variant: "destructive", title: result.message, description: "Failed to verify email" });
-        // console.error("Failed to verify email");
       }
     } catch (error) {
       console.error(error);
       setError(true);
       setLoading(false);
       toast({ variant: "destructive", title: "Something went wrong", description: "Failed to run verify email" });
-      // console.error("Failed to run verify email");
     }
   };
 
@@ -75,15 +82,15 @@ const VerifyEmail = () => {
     <div className="flex justify-center items-center h-screen">
       <div className="w-full max-w-md">
         {verified && (
-          <div>
+          <div className="max-w-md mx-auto">
             <Alert variant="default" className="mb-5">
-            <SquareCheckBig color="green" />
-            <AlertTitle>Email Verified!</AlertTitle>
-            <AlertDescription>
-              Your email has been verified successfully.
-            </AlertDescription>
-          </Alert>
-          <Link className="" href="/login">Proceed to Login</Link>
+              <SquareCheckBig color="green" />
+              <AlertTitle>Email Verified!</AlertTitle>
+              <AlertDescription>
+                Your email has been verified successfully.
+              </AlertDescription>
+            </Alert>
+            <Link className="bg-indigo-900 rounded-md p-4 text-white" href="/login">Proceed to Login</Link>
           </div>
         )}
 
